8f91d746355475278f71e773c59e150bf0dada06
Benedikt Rollik <brollik@scaleway.com>
2023-01-04 16:03:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,8 @@ fi
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 
-echo "[default]
+cat << EOF > ~/.aws/credentials
+[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 
@@ -46,7 +47,24 @@ s3 =
   # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (1000 requests * 10MB). For example setting it to 5GB allows you to upload files up to 5TB.
   multipart_chunksize = 10MB
 s3api =
-  endpoint_url = https://s3.fr-par.scw.cloud" > ~/.aws/credentials
+  endpoint_url = https://s3.fr-par.scw.cloud
+EOF
+
+#echo "[default]
+#aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+#aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+#
+#region = fr-par
+#s3 =
+#  endpoint_url = https://s3.fr-par.scw.cloud
+#  signature_version = s3v4
+#  max_concurrent_requests = 100
+#  max_queue_size = 1000
+#  multipart_threshold = 50MB
+#  # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (1000 requests * 10MB). For example setting it to 5GB allows you to upload files up to 5TB.
+#  multipart_chunksize = 10MB
+#s3api =
+#  endpoint_url = https://s3.fr-par.scw.cloud" > ~/.aws/credentials
 
 
 echo "Change directory to Source"
@@ -63,7 +81,7 @@ echo "Run yarn build"
 yarn run build
 
 echo "Copying to website folder"
-aws s3 sync ./build/${PROJECT_NAME} s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+aws s3 sync ./build/ s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 
 echo "Cleaning up things"
 
